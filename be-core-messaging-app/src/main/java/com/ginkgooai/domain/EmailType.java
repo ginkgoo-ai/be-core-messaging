df0a503c2d2a3ece7c86d68f83a1568b716215c2
Wang <645565009@qq.com>
2025-03-28 10:50:03 +0800

    feat: email service openai config

--- be-core-messaging-app/src/main/java/com/ginkgooai/domain/EmailType.java
+++ be-core-messaging-app/src/main/java/com/ginkgooai/domain/EmailType.java
@@ -1,6 +1,13 @@
 package com.ginkgooai.domain;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "Types of email templates available in the system")
 public enum EmailType {
+    
+    @Schema(description = "Email template for sending invitations")
     INVITATION,
+    
+    @Schema(description = "Email template for inviting users to upload content")
     INVITE_UPLOAD
 }
