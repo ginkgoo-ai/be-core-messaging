7843e23cf3c8971a278b22bad530ed4281043c89
Wang <645565009@qq.com>
2025-04-08 14:59:19 +0800

    feat: sendgrid webhook test

--- be-core-messaging-app/src/main/java/com/ginkgooai/controller/WebhookController.java
+++ be-core-messaging-app/src/main/java/com/ginkgooai/controller/WebhookController.java
@@ -3,10 +3,12 @@ package com.ginkgooai.controller;
 
 import com.alibaba.fastjson.JSON;
 import com.ginkgooai.dto.InboundParseRequest;
-import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -17,21 +19,21 @@ import java.util.List;
 public class WebhookController {
 
 
-    @PostMapping(value = "/webhook", consumes = "multipart/form-data")
+    @PostMapping(consumes = "multipart/form-data")
     public ResponseEntity<String> handleInboundParse(
             // 解析表单字段（使用@RequestPart绑定到对象属性）
-            @Valid @RequestPart("headers") String headers,
+            @RequestPart(value = "headers",required = false) String headers,
             @RequestPart(value = "dkim", required = false) String dkim,
             @RequestPart(value = "content-ids", required = false) List<String> contentIds,
-            @RequestPart("to") String to,
-            @RequestPart("from") String from,
+            @RequestPart(value = "to",required = false) String to,
+            @RequestPart(value = "from",required = false) String from,
             @RequestPart(value = "html", required = false) String html,
             @RequestPart(value = "text", required = false) String text,
             @RequestPart(value = "sender_ip", required = false) String senderIp,
             @RequestPart(value = "spam_report", required = false) String spamReport,
             @RequestPart(value = "envelope", required = false) String envelope,
             @RequestPart(value = "attachments", required = false) List<MultipartFile> attachments,
-            @RequestPart("subject") String subject,
+            @RequestPart(value = "subject",required = false) String subject,
             @RequestPart(value = "spam_score", required = false) String spamScore,
             @RequestPart(value = "attachment-info", required = false) String attachmentInfo,
             @RequestPart(value = "charsets", required = false) String charsets,
