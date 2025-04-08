6e5b4faf540ebe748b15e114989693ca399a2347
Wang <645565009@qq.com>
2025-04-08 11:31:47 +0800

    feat: sendgrid webhook test

--- be-core-messaging-app/src/main/java/com/ginkgooai/controller/WebhookController.java
+++ be-core-messaging-app/src/main/java/com/ginkgooai/controller/WebhookController.java
@@ -2,10 +2,7 @@ package com.ginkgooai.controller;
 
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/webhook")
@@ -13,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class WebhookController {
 
 
-    @GetMapping
+    @PostMapping
     public String handleInboundEmail(
             @RequestParam("from") String from,
             @RequestParam("to") String to,
