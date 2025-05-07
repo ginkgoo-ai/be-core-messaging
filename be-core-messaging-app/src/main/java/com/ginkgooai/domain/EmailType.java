1c846ab1d85ce4714610487101af7ebb0ccc5315
seasonsolt <seasonsolt@gmail.com>
2025-05-07 17:42:57 +0800

    feat:add SHARE_SHORTLIST EmailType

--- be-core-messaging-app/src/main/java/com/ginkgooai/domain/EmailType.java
+++ be-core-messaging-app/src/main/java/com/ginkgooai/domain/EmailType.java
@@ -4,10 +4,14 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "Types of email templates available in the system")
 public enum EmailType {
-    
-    @Schema(description = "Email template for sending invitations")
-    INVITATION,
-    
-    @Schema(description = "Email template for inviting users to upload content")
-    INVITE_UPLOAD
+
+	@Schema(description = "Email template for sending invitations")
+	INVITATION,
+
+	@Schema(description = "Email template for inviting users to upload content")
+	INVITE_UPLOAD,
+
+	@Schema(description = "Email template for share shortlist")
+	SHARE_SHORTLIST
+
 }
